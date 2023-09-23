e439f39afeca483c506ff753543e61b13030cb33
Steve Curtis <steve.curtis@truthmd.com>
2023-09-23 11:19:26 -0400

    add checkstyle and configure version properties in pom

--- server/src/test/java/com/scurtis/server/ServerApplicationTests.java
+++ server/src/test/java/com/scurtis/server/ServerApplicationTests.java
@@ -1,28 +1,15 @@
 package com.scurtis.server;
 
-import com.scurtis.server.service.ConferenceService;
-import com.scurtis.server.service.TeamService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ServerApplicationTests {
 
-    @Autowired
-    private ConferenceService conferenceService;
-
-    @Autowired
-    private TeamService teamService;
-
     @Test
     void contextLoads() {
-        assertNotNull(conferenceService);
-        assertNotNull(teamService);
     }
 
 }
